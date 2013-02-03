da801883aafd8f8731cb33f63df2cf374aceaff8
Martin Stekl <martin.stekl@gmail.com>
2013-02-03 22:37:53 +0100

    Added test directory with bootstrap script

---
+++ tests/bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @author Martin Štekl <martin.stekl@gmail.com>
+ * @since 2013-02-03
+ */
+
+use Nette\Config\Configurator;
+
+define('TEST_DIR', __DIR__);
+define('LIBS_DIR', TEST_DIR . '/../vendor');
+
+// Composer autoloading
+require LIBS_DIR . '/autoload.php';
+
+// Configure application
+$configurator = new Configurator();
+
+// Enable Nette Debugger for error visualisation & logging
+$configurator->setDebugMode(Configurator::AUTO);
+$configurator->enableDebugger(TEST_DIR . '/log', 'martin.stekl@gmail.com');
+
+// Enable RobotLoader - this will load all classes automatically
+$configurator->setTempDirectory(TEST_DIR . '/temp');
+
+$configurator->createRobotLoader()
+	->addDirectory(TEST_DIR)
+	->register();
