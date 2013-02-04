91333155b7da2ba6190cee5a8fb1c968915db054
Martin Stekl <martin.stekl@gmail.com>
2013-02-04 20:31:48 +0100

    Added skeleton for timestamp file storage

---
+++ Cronner/TimestampStorage/FileStorage.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace stekycz\Cronner\TimestampStorage;
+
+use Nette\Object;
+use DateTime;
+use stekycz\Cronner\ITimestampStorage;
+
+/**
+ * @author Martin Štekl <martin.stekl@gmail.com>
+ * @since 2013-02-04
+ */
+class FileStorage extends Object implements ITimestampStorage {
+
+	/**
+	 * @var string
+	 */
+	private $path;
+
+	/**
+	 * @param string $path
+	 */
+	public function __construct($path) {
+		$this->path = $path;
+	}
+
+	/**
+	 * Saves current date and time as last invocation time.
+	 *
+	 * @param \DateTime $now
+	 */
+	public function saveRunTime(DateTime $now) {
+		// TODO - save current date & time
+	}
+
+	/**
+	 * Returns date and time of last cron task invocation.
+	 *
+	 * @return \DateTime|null
+	 */
+	public function loadLastRunTime() {
+		// TODO - load date & time of last invocation
+		return null;
+	}
+
+}
