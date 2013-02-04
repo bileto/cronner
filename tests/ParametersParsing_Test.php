e32be3b66e60fd1ccb5464442d6c495f0ede0306
Martin Stekl <martin.stekl@gmail.com>
2013-02-04 20:30:05 +0100

    Test: added parameters parsing test

---
+++ tests/ParametersParsing_Test.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace stekycz\Cronner\tests;
+
+require_once(TEST_DIR . '/objects/TestObject.php');
+
+use PHPUnit_Framework_TestCase;
+use stekycz\Cronner\tests\objects\TestObject;
+use Nette\Reflection\ClassType;
+use stekycz\Cronner\Tasks\Parameters;
+
+/**
+ * @author Martin Štekl <martin.stekl@gmail.com>
+ * @since 2013-02-04
+ */
+class ParametersParsing_Test extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @var \stekycz\Cronner\Tasks
+	 */
+	private $object;
+
+	protected function setUp() {
+		parent::setUp();
+		$this->object = new TestObject();
+	}
+
+	/**
+	 * @dataProvider dataProviderParse
+	 * @param array $expected
+	 * @param string $methodName
+	 */
+	public function testParse(array $expected, $methodName) {
+		if (!$this->object->getReflection()->hasMethod($methodName)) {
+			$this->fail('Tested class does not have method "' . $methodName . '".');
+			return;
+		}
+
+		$this->assertSame($expected, Parameters::parseParameters($this->object->getReflection()->getMethod($methodName)));
+	}
+
+	public function dataProviderParse() {
+		return array(
+			array(array(
+					Parameters::TASK => 'E-mail notifications',
+					Parameters::PERIOD => '5 minutes',
+					Parameters::DAYS => null,
+					Parameters::TIME => null,
+				), 'test01'
+			),
+			array(array(
+					Parameters::TASK => 'stekycz\Cronner\tests\objects\TestObject - test02',
+					Parameters::PERIOD => '1 hour',
+					Parameters::DAYS => array('Mo', 'We', 'Fr', ),
+					Parameters::TIME => array(
+						array(
+							'from' => '09:00',
+							'to' => '10:00',
+						),
+						array(
+							'from' => '15:00',
+							'to' => '16:00',
+						),
+					),
+				), 'test02'
+			),
+		);
+	}
+
+}
--- tests/objects/TestObject.php
+++ tests/objects/TestObject.php
@@ -8,13 +8,22 @@ use stekycz\Cronner\Tasks;
  * @author Martin Štekl <martin.stekl@gmail.com>
  * @since 2013-02-03
  */
-class TestTasks extends Tasks {
+class TestObject extends Tasks {
 
 	/**
 	 * @cronner-task E-mail notifications
 	 * @cronner-period 5 minutes
 	 */
-	public function sendEmailNotifications() {
+	public function test01() {
+	}
+
+	/**
+	 * @cronner-task
+	 * @cronner-period 1 hour
+	 * @cronner-days [Mo, We, Fr]
+	 * @cronner-time [09:00 - 10:00, 15:00 - 16:00]
+	 */
+	public function test02() {
 	}
 
 }
