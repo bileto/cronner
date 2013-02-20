bf532d7c3da2ac72351b53e7ddb7f7fed625cb33
Martin Stekl <martin.stekl@gmail.com>
2013-02-20 23:57:03 +0100

    Test: Added test for Parameters::isInDay() and extended data for one other test

--- tests/Tasks/Parameters_Test.php
+++ tests/Tasks/Parameters_Test.php
@@ -26,15 +26,15 @@ class Parameters_Test extends PHPUnit_Framework_TestCase {
 
 	public function dataProviderGetName() {
 		return array(
-			array('Name of task', array (Parameters::TASK => 'Name of task',)),
-			array('0', array (Parameters::TASK => '0',)),
-			array('', array (Parameters::TASK => '   ',)),
-			array('', array (Parameters::TASK => '',)),
-			array('', array (Parameters::TASK => true,)),
-			array('', array (Parameters::TASK => false,)),
-			array('', array (Parameters::TASK => null,)),
-			array('', array (Parameters::TASK => 0,)),
-			array('', array ()),
+			array('Name of task', array(Parameters::TASK => 'Name of task',)),
+			array('0', array(Parameters::TASK => '0',)),
+			array('', array(Parameters::TASK => '   ',)),
+			array('', array(Parameters::TASK => '',)),
+			array('', array(Parameters::TASK => true,)),
+			array('', array(Parameters::TASK => false,)),
+			array('', array(Parameters::TASK => null,)),
+			array('', array(Parameters::TASK => 0,)),
+			array('', array()),
 		);
 	}
 
@@ -51,15 +51,15 @@ class Parameters_Test extends PHPUnit_Framework_TestCase {
 
 	public function dataProviderIsTask() {
 		return array(
-			array(true, array (Parameters::TASK => 'Name of task',)),
-			array(true, array (Parameters::TASK => '0',)),
-			array(false, array (Parameters::TASK => '   ',)),
-			array(false, array (Parameters::TASK => '',)),
-			array(false, array (Parameters::TASK => true,)),
-			array(false, array (Parameters::TASK => false,)),
-			array(false, array (Parameters::TASK => null,)),
-			array(false, array (Parameters::TASK => 0,)),
-			array(false, array ()),
+			array(true, array(Parameters::TASK => 'Name of task',)),
+			array(true, array(Parameters::TASK => '0',)),
+			array(false, array(Parameters::TASK => '   ',)),
+			array(false, array(Parameters::TASK => '',)),
+			array(false, array(Parameters::TASK => true,)),
+			array(false, array(Parameters::TASK => false,)),
+			array(false, array(Parameters::TASK => null,)),
+			array(false, array(Parameters::TASK => 0,)),
+			array(false, array()),
 		);
 	}
 
@@ -68,46 +68,112 @@ class Parameters_Test extends PHPUnit_Framework_TestCase {
 	 * @dataProvider dataProviderIsNextPeriod
 	 * @param bool $expected
 	 * @param \DateTime $now
-	 * @param \DateTime $lastRunTime
+	 * @param \DateTime|null $lastRunTime
 	 * @param array $parameters
 	 */
-	public function detectsIfNowIsInNextPeriod($expected, DateTime $now, DateTime $lastRunTime, array $parameters) {
+	public function detectsIfNowIsInNextPeriod($expected, DateTime $now, DateTime $lastRunTime = null, array $parameters) {
 		$params = new Parameters($parameters);
 		$this->assertEquals($expected, $params->isNextPeriod($now, $lastRunTime));
 	}
 
 	public function dataProviderIsNextPeriod() {
 		return array(
-			array(true,
+			array(
+				true,
 				new Nette\DateTime('2013-02-03 17:00:00'),
 				new Nette\DateTime('2013-02-03 16:54:59'),
-				array (Parameters::PERIOD => '5 minutes',)
+				array(Parameters::PERIOD => '5 minutes',)
 			),
-			array(true,
+			array(
+				true,
 				new Nette\DateTime('2013-02-03 17:00:00'),
 				new Nette\DateTime('2013-02-03 16:55:00'),
-				array (Parameters::PERIOD => '5 minutes',)
+				array(Parameters::PERIOD => '5 minutes',)
 			),
-			array(false,
+			array(
+				false,
 				new Nette\DateTime('2013-02-03 17:00:00'),
 				new Nette\DateTime('2013-02-03 16:55:01'),
-				array (Parameters::PERIOD => '5 minutes',)
+				array(Parameters::PERIOD => '5 minutes',)
 			),
-			array(false,
+			array(
+				false,
 				new Nette\DateTime('2013-02-03 17:00:00'),
 				new Nette\DateTime('2013-02-03 16:55:01'),
-				array (Parameters::PERIOD => '1 hour',)
+				array(Parameters::PERIOD => '1 hour',)
 			),
-			array(true,
+			array(
+				true,
 				new Nette\DateTime('2013-02-03 17:00:00'),
 				new Nette\DateTime('2013-02-03 16:00:00'),
-				array (Parameters::PERIOD => '1 hour',)
+				array(Parameters::PERIOD => '1 hour',)
 			),
-			array(true,
+			array(
+				true,
 				new Nette\DateTime('2013-02-03 17:00:00'),
 				new Nette\DateTime('2013-02-03 16:00:00'),
-				array ()
+				array()
 			),
+			array(
+				true,
+				new Nette\DateTime('2013-02-03 17:00:00'),
+				null,
+				array(Parameters::PERIOD => '1 hour',)
+			),
+			array(
+				true,
+				new Nette\DateTime('2013-02-03 17:00:00'),
+				null,
+				array()
+			),
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider dataProviderIsInDay
+	 * @param bool $expected
+	 * @param array $parameters
+	 * @param \DateTime $now
+	 */
+	public function detectsAllowedDaysOfWeek($expected, array $parameters, DateTime $now) {
+		$params = new Parameters($parameters);
+		$this->assertEquals($expected, $params->isInDay($now));
+	}
+
+	public function dataProviderIsInDay() {
+		return array(
+			// One day
+			array(true, array(Parameters::DAYS => array('Mon',),), new Nette\DateTime('2013-02-11 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Mon',),), new Nette\DateTime('2013-02-12 12:34:56')),
+			array(true, array(Parameters::DAYS => array('Tue',),), new Nette\DateTime('2013-02-12 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Tue',),), new Nette\DateTime('2013-02-13 12:34:56')),
+			array(true, array(Parameters::DAYS => array('Wed',),), new Nette\DateTime('2013-02-13 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Wed',),), new Nette\DateTime('2013-02-14 12:34:56')),
+			array(true, array(Parameters::DAYS => array('Thu',),), new Nette\DateTime('2013-02-14 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Thu',),), new Nette\DateTime('2013-02-15 12:34:56')),
+			array(true, array(Parameters::DAYS => array('Fri',),), new Nette\DateTime('2013-02-15 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Fri',),), new Nette\DateTime('2013-02-16 12:34:56')),
+			array(true, array(Parameters::DAYS => array('Sat',),), new Nette\DateTime('2013-02-16 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Sat',),), new Nette\DateTime('2013-02-17 12:34:56')),
+			array(true, array(Parameters::DAYS => array('Sun',),), new Nette\DateTime('2013-02-17 12:34:56')),
+			array(false, array(Parameters::DAYS => array('Sun',),), new Nette\DateTime('2013-02-18 12:34:56')),
+			// Empty days
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-11 12:34:56')),
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-12 12:34:56')),
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-13 12:34:56')),
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-14 12:34:56')),
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-15 12:34:56')),
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-16 12:34:56')),
+			array(false, array(Parameters::DAYS => array(),), new Nette\DateTime('2013-02-17 12:34:56')),
+			// Without days
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-11 12:34:56')),
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-12 12:34:56')),
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-13 12:34:56')),
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-14 12:34:56')),
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-15 12:34:56')),
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-16 12:34:56')),
+			array(true, array(Parameters::DAYS => null,), new Nette\DateTime('2013-02-17 12:34:56')),
 		);
 	}
 
