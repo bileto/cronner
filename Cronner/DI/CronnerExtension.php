c6d7c597eb387be736582424b1d2ab20ef5d0810
Martin Štekl <martin.stekl@gmail.com>
2014-08-17 15:14:51 +0200

    Added tag for autoregistration of tasks

--- Cronner/DI/CronnerExtension.php
+++ Cronner/DI/CronnerExtension.php
@@ -21,6 +21,8 @@ if (!class_exists('Nette\DI\CompilerExtension')) {
 class CronnerExtension extends CompilerExtension
 {
 
+	const TASKS_TAG = 'cronner.tasks';
+
 	/**
 	 * @var array
 	 */
@@ -56,13 +58,17 @@ class CronnerExtension extends CompilerExtension
 			->setAutowired(FALSE)
 			->setInject(FALSE);
 
-		$container->addDefinition($this->prefix('runner'))
+		$runner = $container->addDefinition($this->prefix('runner'))
 			->setClass('stekycz\Cronner\Cronner', array(
 				$storage,
 				$criticalSection,
 				$config['maxExecutionTime'],
 				!$config['debugMode'],
 			));
+
+		foreach (array_keys($container->findByTag(self::TASKS_TAG)) as $serviceName) {
+			$runner->addSetup('addTasks', array('@' . $serviceName));
+		}
 	}
 
 
