65b14512043ab38b7c3a07a6a4077ddfad30e096
Martin Štekl <martin.stekl@gmail.com>
2014-08-19 00:14:42 +0200

    Removed purge of temp directory

--- tests/CronnerTests/bootstrap.php
+++ tests/CronnerTests/bootstrap.php
@@ -6,7 +6,6 @@ $autoloader = require_once __DIR__ . '/../../vendor/autoload.php';
 
 define("TEST_DIR", __DIR__);
 define("TEMP_DIR", TEST_DIR . '/../tmp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
-Tester\Helpers::purge(TEMP_DIR);
 Tester\Environment::setup();
 
 function run(Tester\TestCase $testCase)
